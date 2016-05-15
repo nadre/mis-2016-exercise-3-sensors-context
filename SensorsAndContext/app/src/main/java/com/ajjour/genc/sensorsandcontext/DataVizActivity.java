13a015c45ee5e6770ac4a2e75866288fc4700508
Erdan Genc <erdan.genc@gmail.com>
2016-05-15 12:54:10 +0200

    adding accelometer viz

--- SensorsAndContext/app/src/main/java/com/ajjour/genc/sensorsandcontext/DataVizActivity.java
+++ SensorsAndContext/app/src/main/java/com/ajjour/genc/sensorsandcontext/DataVizActivity.java
@@ -1,11 +1,26 @@
 package com.ajjour.genc.sensorsandcontext;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-public class DataVizActivity extends AppCompatActivity {
+/**
+ * Created by neffle on 15.05.16.
+ *
+ * Inspiration:
+ * https://developer.android.com/guide/topics/sensors/sensors_overview.html
+ */
+public class DataVizActivity extends AppCompatActivity implements SensorEventListener {
 
     private DataVizView dataVizView;
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,10 +28,40 @@ public class DataVizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_data_viz);
 
         dataVizView = (DataVizView) findViewById(R.id.data_viz_canvas);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        if (accelerometer == null){
+            Toast.makeText(this, "Whoopsie, couldn't find Accelerometer.", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void clearCanvas(){
         dataVizView.clearCanvas();
     }
 
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float[] values = sensorEvent.values;
+        dataVizView.updateView(values);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
 }
--- SensorsAndContext/app/src/main/java/com/ajjour/genc/sensorsandcontext/DataVizView.java
+++ SensorsAndContext/app/src/main/java/com/ajjour/genc/sensorsandcontext/DataVizView.java
@@ -3,7 +3,9 @@ package com.ajjour.genc.sensorsandcontext;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -21,12 +23,42 @@ public class DataVizView extends View {
     Canvas canvas;
     Bitmap bitmap;
 
+    Path[] paths;
+    Paint[] paints;
+
+    float[] values;
+    float T;
+    float dx;
+    float dy;
+    long startTimeMillis;
+
     public DataVizView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         this.context = context;
         this.attrs = attrs;
 
+        paths = new Path[4];
+        paints = new Paint[4];
+
+        Paint paint = new Paint();
+
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeWidth(4f);
+
+        for (int i = 0; i < 4; i++){
+            paints[i] = new Paint(paint);
+            paths[i] = new Path();
+        }
+
+        paints[0].setColor(Color.RED);
+        paints[1].setColor(Color.GREEN);
+        paints[2].setColor(Color.BLUE);
+        paints[3].setColor(Color.BLACK);
+
+        values = new float[4];
     }
 
     @Override
@@ -34,20 +66,47 @@ public class DataVizView extends View {
         super.onSizeChanged(w, h, oldw, oldh);
         bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
         canvas = new Canvas(bitmap);
+        startTimeMillis = System.currentTimeMillis();
+
+        dx = this.getHeight() / 2;
+        dy = this.getWidth() / 2;
+        T = 0;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-
-        Paint paint = new Paint();
-        paint.setARGB(100, 100, 100, 100);
-
         super.onDraw(canvas);
-        canvas.drawCircle(100, 100, 100, paint);
+
+        for (int i = 0; i < 4; i++){
+            canvas.drawPath(paths[i], paints[i]);
+        }
+
+
     }
 
     public void clearCanvas() {
-        /*TODO: Clear something*/
+        for (int i = 0; i < 4; i++){
+            paths[i].reset();
+        }
+        invalidate();
+    }
+
+    public void updateView(float newValues[]) {
+
+        float newVal;
+        float oldVal;
+
+        long t = (System.currentTimeMillis() - startTimeMillis) / 10;
+
+        for (int i = 0; i < 3; i++){
+            newVal = newValues[i];
+            newVal = (newVal * 10) + dx;
+            oldVal = this.values[i];
+            paths[i].quadTo(T, oldVal, (t + T) / 2, (newVal + oldVal) / 2);
+            this.values[i] = newVal;
+        }
+        T = t;
+
         invalidate();
     }
 }
